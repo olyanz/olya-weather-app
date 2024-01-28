ef78e4665b0a1cd17dd44b4997dce8e370d40c48
olyanz <151459992+olyanz@users.noreply.github.com>
2024-01-28 14:53:02 +0100

    Updated js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
 function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
+  let currentTempValue = document.querySelector("#current-temperature");
+  currentTempValue.innerHTML = temperature;
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
 }
 
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
-
+  let searchInput = searchInputElement.value;
   let apiKey = "b2a5adcct04b33178913oc335f405433";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInput}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
